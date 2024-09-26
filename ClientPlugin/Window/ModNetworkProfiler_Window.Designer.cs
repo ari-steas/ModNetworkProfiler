c1fad357e3e2958807d82fdf743a415a37c92f6b
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-09-26 15:26:43 -0500

    Window Formatting Edits

--- ClientPlugin/Window/ModNetworkProfiler_Window.Designer.cs
+++ ClientPlugin/Window/ModNetworkProfiler_Window.Designer.cs
@@ -230,8 +230,8 @@ namespace ClientPlugin.Window
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.NetworkDownTitle = new System.Windows.Forms.Label();
             this.NetworkDownList = new System.Windows.Forms.TreeView();
             this.DebugLabel = new System.Windows.Forms.Label();
@@ -240,16 +240,19 @@ namespace ClientPlugin.Window
             this.ShowAllButton = new System.Windows.Forms.Button();
             this.playButton = new System.Windows.Forms.Button();
             this.pauseButton = new System.Windows.Forms.Button();
+            this.ListLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.GraphLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.ListLayoutPanel.SuspendLayout();
+            this.GraphLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // NetworkDownTitle
             // 
             this.NetworkDownTitle.AutoSize = true;
-            this.NetworkDownTitle.Location = new System.Drawing.Point(40, 35);
-            this.NetworkDownTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.NetworkDownTitle.Location = new System.Drawing.Point(3, 0);
             this.NetworkDownTitle.Name = "NetworkDownTitle";
-            this.NetworkDownTitle.Size = new System.Drawing.Size(78, 13);
+            this.NetworkDownTitle.Size = new System.Drawing.Size(93, 16);
             this.NetworkDownTitle.TabIndex = 0;
             this.NetworkDownTitle.Text = "Network Down";
             this.NetworkDownTitle.Click += new System.EventHandler(this.label1_Click);
@@ -259,19 +262,18 @@ namespace ClientPlugin.Window
             this.NetworkDownList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.NetworkDownList.ForeColor = System.Drawing.Color.White;
             this.NetworkDownList.LineColor = System.Drawing.Color.White;
-            this.NetworkDownList.Location = new System.Drawing.Point(43, 67);
-            this.NetworkDownList.Margin = new System.Windows.Forms.Padding(2);
+            this.NetworkDownList.Location = new System.Drawing.Point(3, 18);
+            this.NetworkDownList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NetworkDownList.Name = "NetworkDownList";
-            this.NetworkDownList.Size = new System.Drawing.Size(474, 586);
+            this.NetworkDownList.Size = new System.Drawing.Size(631, 720);
             this.NetworkDownList.TabIndex = 1;
             // 
             // DebugLabel
             // 
             this.DebugLabel.AutoSize = true;
-            this.DebugLabel.Location = new System.Drawing.Point(589, 657);
-            this.DebugLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DebugLabel.Location = new System.Drawing.Point(3, 724);
             this.DebugLabel.Name = "DebugLabel";
-            this.DebugLabel.Size = new System.Drawing.Size(66, 13);
+            this.DebugLabel.Size = new System.Drawing.Size(82, 16);
             this.DebugLabel.TabIndex = 2;
             this.DebugLabel.Text = "Errors: None";
             this.DebugLabel.Click += new System.EventHandler(this.label1_Click_1);
@@ -281,16 +283,16 @@ namespace ClientPlugin.Window
             this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.chart1.BackSecondaryColor = System.Drawing.Color.Transparent;
             this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Cursor = System.Windows.Forms.Cursors.Default;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(591, 67);
-            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(3, 2);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
-            this.chart1.Size = new System.Drawing.Size(544, 585);
+            this.chart1.Size = new System.Drawing.Size(725, 720);
             this.chart1.TabIndex = 3;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
@@ -299,10 +301,10 @@ namespace ClientPlugin.Window
             // 
             this.HideAllButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.HideAllButton.ForeColor = System.Drawing.Color.White;
-            this.HideAllButton.Location = new System.Drawing.Point(43, 658);
-            this.HideAllButton.Margin = new System.Windows.Forms.Padding(2);
+            this.HideAllButton.Location = new System.Drawing.Point(328, 744);
+            this.HideAllButton.Margin = new System.Windows.Forms.Padding(4);
             this.HideAllButton.Name = "HideAllButton";
-            this.HideAllButton.Size = new System.Drawing.Size(68, 19);
+            this.HideAllButton.Size = new System.Drawing.Size(100, 28);
             this.HideAllButton.TabIndex = 4;
             this.HideAllButton.Text = "Hide All";
             this.HideAllButton.UseVisualStyleBackColor = false;
@@ -312,10 +314,10 @@ namespace ClientPlugin.Window
             // 
             this.ShowAllButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ShowAllButton.ForeColor = System.Drawing.Color.White;
-            this.ShowAllButton.Location = new System.Drawing.Point(115, 657);
-            this.ShowAllButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ShowAllButton.Location = new System.Drawing.Point(220, 744);
+            this.ShowAllButton.Margin = new System.Windows.Forms.Padding(4);
             this.ShowAllButton.Name = "ShowAllButton";
-            this.ShowAllButton.Size = new System.Drawing.Size(68, 19);
+            this.ShowAllButton.Size = new System.Drawing.Size(100, 28);
             this.ShowAllButton.TabIndex = 5;
             this.ShowAllButton.Text = "Expand All";
             this.ShowAllButton.UseVisualStyleBackColor = false;
@@ -323,48 +325,71 @@ namespace ClientPlugin.Window
             // 
             // playButton
             // 
-            this.playButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.playButton.Location = new System.Drawing.Point(221, 657);
+            this.playButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(112, 744);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.Size = new System.Drawing.Size(100, 28);
             this.playButton.TabIndex = 6;
             this.playButton.Text = "PLAY";
-            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.UseVisualStyleBackColor = false;
             this.playButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // pauseButton
             // 
-            this.pauseButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.pauseButton.Location = new System.Drawing.Point(303, 657);
+            this.pauseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.pauseButton.ForeColor = System.Drawing.Color.White;
+            this.pauseButton.Location = new System.Drawing.Point(4, 744);
+            this.pauseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pauseButton.Name = "pauseButton";
-            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.Size = new System.Drawing.Size(100, 28);
             this.pauseButton.TabIndex = 7;
             this.pauseButton.Text = "PAUSE";
-            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.UseVisualStyleBackColor = false;
             this.pauseButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ListLayoutPanel
+            // 
+            this.ListLayoutPanel.Controls.Add(this.NetworkDownTitle);
+            this.ListLayoutPanel.Controls.Add(this.NetworkDownList);
+            this.ListLayoutPanel.Controls.Add(this.pauseButton);
+            this.ListLayoutPanel.Controls.Add(this.playButton);
+            this.ListLayoutPanel.Controls.Add(this.ShowAllButton);
+            this.ListLayoutPanel.Controls.Add(this.HideAllButton);
+            this.ListLayoutPanel.Location = new System.Drawing.Point(57, 43);
+            this.ListLayoutPanel.Name = "ListLayoutPanel";
+            this.ListLayoutPanel.Size = new System.Drawing.Size(631, 794);
+            this.ListLayoutPanel.TabIndex = 8;
+            // 
+            // GraphLayoutPanel
+            // 
+            this.GraphLayoutPanel.Controls.Add(this.chart1);
+            this.GraphLayoutPanel.Controls.Add(this.DebugLabel);
+            this.GraphLayoutPanel.Location = new System.Drawing.Point(788, 43);
+            this.GraphLayoutPanel.Name = "GraphLayoutPanel";
+            this.GraphLayoutPanel.Size = new System.Drawing.Size(725, 794);
+            this.GraphLayoutPanel.TabIndex = 9;
             // 
             // ModNetworkProfiler_Window
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.ClientSize = new System.Drawing.Size(1185, 714);
-            this.Controls.Add(this.pauseButton);
-            this.Controls.Add(this.playButton);
-            this.Controls.Add(this.ShowAllButton);
-            this.Controls.Add(this.HideAllButton);
-            this.Controls.Add(this.chart1);
-            this.Controls.Add(this.DebugLabel);
-            this.Controls.Add(this.NetworkDownList);
-            this.Controls.Add(this.NetworkDownTitle);
+            this.ClientSize = new System.Drawing.Size(1580, 879);
+            this.Controls.Add(this.ListLayoutPanel);
+            this.Controls.Add(this.GraphLayoutPanel);
             this.ForeColor = System.Drawing.SystemColors.Control;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ModNetworkProfiler_Window";
             this.Text = "ModNetworkProfiler";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ListLayoutPanel.ResumeLayout(false);
+            this.ListLayoutPanel.PerformLayout();
+            this.GraphLayoutPanel.ResumeLayout(false);
+            this.GraphLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -378,5 +403,7 @@ namespace ClientPlugin.Window
         private Button ShowAllButton;
         private Button playButton;
         private Button pauseButton;
+        private FlowLayoutPanel ListLayoutPanel;
+        private FlowLayoutPanel GraphLayoutPanel;
     }
 }
